0f14e0432d55dbce5abdbe0473f26a15bd2295c1
Jack McGill <mr.mcgilljh@gmail.com>
2022-12-01 20:40:45 +1000

    Day 01

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::str::Lines;
+
+fn main() {
+    let data: String = fs::read_to_string("src/input.txt").expect("Couldn't read in file");
+    let lines: Lines = data.lines();
+
+    let mut cals: i32 = 0;
+    let mut elves: Vec<i32> = Vec::new();
+
+    for line in lines {
+        if !line.is_empty() {
+            cals += to_i32(line);
+        } else {
+            elves.push(cals);
+            cals = 0;
+        }
+    }
+
+    let max: &i32 = elves.iter().max().unwrap();
+    println!("{max}"); // PART ONE
+
+    elves.sort();
+    elves.reverse();
+
+    let x: i32 = elves[0] + elves[1] + elves[2];
+    println!("{x}"); // PART TWO
+}
+
+fn to_i32(x: &str) -> i32 {
+    x.parse::<i32>().unwrap()
+}
